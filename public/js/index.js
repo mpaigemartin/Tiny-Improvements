adcf211bce919433179403ba9873b55c60a96170
Paige <mpaigemartin@gmail.com>
2019-03-30 09:42:26 -0400

    give kudos button doesn't do anything

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,9 @@
-const render = function(dataList) {
-  $("#kudos").empty();
-  for (let i = 0; i < dataList.length; i++) {
-    $("#kudos").append(
-      `<div class="card">
+$(function() {
+  const render = function(dataList) {
+    $("#kudos").empty();
+    for (let i = 0; i < dataList.length; i++) {
+      $("#kudos").append(
+        `<div class="card">
           <p>${dataList[i].title}</p>
           <p>From: ${dataList[i].from[0].fromUser}</p>
           <div class="body">
@@ -10,66 +11,67 @@ const render = function(dataList) {
             <p>${dataList[i].body}</p>
           </div>
         </div>`
-    );
-  }
-};
-
-const getKudos = function() {
-  $.get("/api/kudo").then(function(data) {
-    render(data);
-  });
-};
-
-const getUsers = function(dataList) {
-  $.get("/api/user/").then(function(data) {
-    for (let i = 0; i < dataList.length; i++) {
-      $("#kudo-from").append(
-        `<option value="${dataList[i]._id}'>${dataList[i].name}</option>`
-      );
-      $("#kudo-to").append(
-        `<option value="${dataList[i]._id}'>${dataList[i].name}</option>`
       );
     }
-  });
-};
+  };
 
-const postKudo = function(e) {
-  e.preventDefault();
-  // $("#messages").empty();
-  console.log("kudos");
+  const getKudos = function() {
+    $.get("/api/kudo").then(function(data) {
+      render(data);
+    });
+  };
 
-  if ($("#kudo-from").val() && $("#kudo-to").val()) {
-    const kudo = {
-      title: $("#kudo-title")
-        .val()
-        .trim(),
-      body: $("#kudo-body")
-        .val()
-        .trim(),
-      from: $("#kudo-from").val(),
-      to: $("#kudo-to").val()
-    };
+  const getUsers = function(dataList) {
+    $.get("/api/user/").then(function(dataList) {
+      for (let i = 0; i < dataList.length; i++) {
+        $("#kudo-from").append(
+          `<option value="${dataList[i]._id}'>${dataList[i].name}</option>`
+        );
+        $("#kudo-to").append(
+          `<option value="${dataList[i]._id}'>${dataList[i].name}</option>`
+        );
+      }
+    });
+  };
 
-    $.post("/api/kudo", kudo)
-      .then(function(data) {
-        $("#kudo-title").val("");
-        $("#kudo-body").val("");
-        $("#kudo-from").val("");
-        $("#kudo-to").val("");
+  const postKudo = function(e) {
+    e.preventDefault();
+    // $("#messages").empty();
+    console.log("kudos");
 
-        $(".modal").modal("hide");
+    if ($("#kudo-from").val() && $("#kudo-to").val()) {
+      const kudo = {
+        title: $("#kudo-title")
+          .val()
+          .trim(),
+        body: $("#kudo-body")
+          .val()
+          .trim(),
+        from: $("#kudo-from").val(),
+        to: $("#kudo-to").val()
+      };
 
-        getKudos();
-      })
-      .fail(function(err) {
-        $("#messages").append(`<p>Error</p>`);
-      });
-  } else {
-    $("#messages").append(`<p>Please select recipients<p>`);
-  }
-};
+      $.post("/api/kudo", kudo)
+        .then(function(data) {
+          $("#kudo-title").val("");
+          $("#kudo-body").val("");
+          $("#kudo-from").val("");
+          $("#kudo-to").val("");
 
-getKudos();
-getUsers();
+          $(".modal").modal("hide");
 
-$(document).on("click", "#send-kudo", postKudo);
+          getKudos();
+        })
+        .fail(function(err) {
+          $("#messages").append(`<p>Error</p>`);
+        });
+    } else {
+      $("#messages").append(`<p>Please select recipients<p>`);
+    }
+  };
+
+  getKudos();
+  getUsers();
+
+  $("#send-kudo").on("click", postKudo);
+});
